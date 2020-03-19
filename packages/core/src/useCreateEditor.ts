750cde7c687d72229c9208c9256ed9adc3f3e65f
Mateusz Hadryś <hadrys.mateusz86@gmail.com>
2020-03-19 16:48:05 +0100

    add withReact to useCreateEditor

--- packages/core/src/useCreateEditor.ts
+++ packages/core/src/useCreateEditor.ts
@@ -1,5 +1,6 @@
 import { useMemo } from "react"
 import { createEditor } from "slate"
+import { withReact } from "slate-react"
 
 import { compose } from "lodash/fp"
 
@@ -19,5 +20,5 @@ export function useCreateEditor(plugins = []) {
 	}, [])
 	/* Compose is a higher-order function returning a function that will be run be the hook - I'm not wrapping it because it would require adding 'plugins' as the hook's dependency which in turn would require making sure the plugins array stays the same on every render to prevent re-creating the editor every time */
 	// TODO: it would be nice to write a custom compose function to remove the need for the lodash dependency
-	return useMemo(compose(...plugins, createEditor), [])
+	return useMemo(compose(...plugins, withReact, createEditor), [])
 }
