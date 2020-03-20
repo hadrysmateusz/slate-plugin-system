7b035777eac86fb88b4c4cd18be908e3fbc5d1d5
Mateusz Hadryś <hadrys.mateusz86@gmail.com>
2020-03-20 02:31:57 +0100

    modify build config to produce source map and type declarations

--- packages/core/webpack.config.js
+++ packages/core/webpack.config.js
@@ -1,11 +1,13 @@
 const path = require("path")
 
 const config = {
+  mode: "production",
   entry: "./src/index.ts",
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "index.js"
   },
+  devtool: "source-map",
   module: {
     rules: [
       {
