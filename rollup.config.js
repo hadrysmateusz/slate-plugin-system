9b876e28762b0c83bec7dbf268af1878293b0713
Mateusz Hadryś <hadrys.mateusz86@gmail.com>
2020-03-21 02:29:51 +0100

    finish move to rollup + new useCreateEditor, no lodash dependency

--- rollup.config.js
+++ rollup.config.js
@@ -1,3 +1,8 @@
+import babel from 'rollup-plugin-babel';
+import commonjs from 'rollup-plugin-commonjs';
+import builtins from 'rollup-plugin-node-builtins';
+import globals from 'rollup-plugin-node-globals';
+import resolve from 'rollup-plugin-node-resolve';
 import typescript from "rollup-plugin-typescript2"
 import pkg from "./package.json"
 
@@ -6,11 +11,17 @@ export default {
   output: [
     {
       file: pkg.main,
-      format: "cjs",sourcemap: true,
+      format: "cjs",
+      sourcemap: true,
+      exports: 'named',
+      name: pkg.name
     },
     {
       file: pkg.module,
-      format: "es",sourcemap: true,
+      format: "es",
+      sourcemap: true,
+      exports: 'named',
+      name: pkg.name
     }
   ],
   external: [
@@ -18,8 +29,60 @@ export default {
     ...Object.keys(pkg.peerDependencies || {})
   ], 
   plugins: [
+    // Allow Rollup to resolve modules from `node_modules`, since it only
+    // resolves local modules by default.
+    resolve({
+      browser: true,
+    }),
+
     typescript({
       typescript: require("typescript")
-    })
+    }),
+
+    // Allow Rollup to resolve CommonJS modules, since it only resolves ES2015
+    // modules by default.
+    commonjs({
+      include: 'node_modules/**',
+      namedExports: {
+        'node_modules/react/index.js': [
+          'cloneElement',
+          'createContext',
+          'Component',
+          'createElement',
+        ],
+        'node_modules/react-dom/index.js': ['render', 'hydrate'],
+        'node_modules/react-is/index.js': [
+          'isElement',
+          'isValidElementType',
+          'ForwardRef',
+        ],
+      },
+    }),
+
+    // Register Node.js builtins for browserify compatibility.
+    builtins(),
+
+    // Use Babel to transpile the result, limiting it to the source code.
+    babel({
+      presets: [
+        [
+          '@babel/preset-env',
+          {
+            modules: false,
+            targets: {
+              node: 'current',
+            },
+          },
+        ],
+        '@babel/preset-react',
+        '@babel/preset-typescript',
+      ],
+      extensions: ['.js', '.jsx', '.es6', '.es', '.mjs', '.ts', '.tsx'],
+      exclude: 'node_modules/**',
+      runtimeHelpers: true,
+    }),
+    
+    // Register Node.js globals for browserify compatibility.
+    globals(),
   ]
 }
--- src/useCreateEditor.ts
+++ src/useCreateEditor.ts
@@ -2,27 +2,21 @@ import { useMemo } from "react"
 import { createEditor } from "slate"
 import { withReact } from "slate-react"
 
-import { compose } from "lodash/fp"
-import { SlatePlugin, EditorOverrides } from "./types"
+import { SlatePlugin } from "./types"
 
 /**
  * Creates the editor object and applies all of the passed-in plugins
  * @param plugins array of plugins
  */
 
-export function useCreateEditor(plugins: SlatePlugin[] = []) {
-  // only get the editorOverride functions from the plugins which have them
-  const editorOverrides = plugins.reduce(
-    (acc: EditorOverrides[], plugin: SlatePlugin) => {
-      if (!plugin.editorOverrides) {
-        return acc
-      } else {
-        return [...acc, plugin.editorOverrides]
+export const useCreateEditor = (plugins: SlatePlugin[] = []) => {
+  return useMemo(()=>{
+    let editor = withReact(createEditor())
+    plugins.forEach(plugin=>{
+			if (plugin.editorOverrides) {
+        editor = plugin.editorOverrides(editor)
       }
-    },
-    []
-  )
-  /* Compose is a higher-order function returning a function that will be run be the hook - I'm not wrapping it because it would require adding 'plugins' as the hook's dependency which in turn would require making sure the plugins array stays the same on every render to prevent re-creating the editor every time */
-  // TODO: it would be nice to write a custom compose function to remove the need for the lodash dependency
-  return useMemo(compose(...editorOverrides, withReact, createEditor), [])
+		})
+    return editor
+  }, [])
 }
