20a3aee71e379ad0d1be65a6537cc519b60fed01
Mateusz Hadryś <hadrys.mateusz86@gmail.com>
2020-03-27 12:08:05 +0100

    change types using Editor to ReactEditor

--- packages/core/src/types.ts
+++ packages/core/src/types.ts
@@ -1,16 +1,16 @@
-import { Editor, NodeEntry, Range } from "slate"
-import { RenderElementProps, RenderLeafProps } from "slate-react"
+import { NodeEntry, Range } from "slate"
+import { RenderElementProps, RenderLeafProps, ReactEditor } from "slate-react"
 
 // Factory Generic for creating Plugin Components
 type Factory<T> = (options?: Object) => T
 
 // Plugin Component Types
-export type EditorOverrides = (editor: Editor) => Editor
-export type OnKeyDown = (e: any, editor: Editor, props?: any) => void
+export type EditorOverrides = (editor: ReactEditor) => ReactEditor
+export type OnKeyDown = (e: any, editor: ReactEditor, props?: any) => void
 export type RenderElement = (props: RenderElementProps) => JSX.Element | undefined
 export type RenderLeaf = (props: RenderLeafProps) => JSX.Element
 export type Decorate = (entry: NodeEntry) => Range[]
-export type OnDOMBeforeInput = (event: Event, editor: Editor) => void
+export type OnDOMBeforeInput = (event: Event, editor: ReactEditor) => void
 
 // Plugin Component Factory Types
 export type EditorOverridesFactory = Factory<EditorOverrides>
@@ -22,40 +22,40 @@ export type OnDOMBeforeInputFactory = Factory<OnDOMBeforeInput>
 
 // Plugin Options
 export interface SlatePlugin {
-	decorate?: Decorate
-	onDOMBeforeInput?: OnDOMBeforeInput
-	renderElement?: RenderElement
-	renderLeaf?: RenderLeaf
-	onKeyDown?: OnKeyDown
-	editorOverrides?: EditorOverrides
+  decorate?: Decorate
+  onDOMBeforeInput?: OnDOMBeforeInput
+  renderElement?: RenderElement
+  renderLeaf?: RenderLeaf
+  onKeyDown?: OnKeyDown
+  editorOverrides?: EditorOverrides
 }
 
 // GetRender Options
 export interface GetRenderLeafOptions {
-	type: string
-	component: any
+  type: string
+  component: any
 }
 export interface GetRenderElementOptions {
-	type: string
-	component: any
+  type: string
+  component: any
 }
 
 // Render Options
 export interface RenderElementOptions {
-	component?: any
+  component?: any
 }
 export interface RenderLeafOptions {
-	component?: any
+  component?: any
 }
 export interface RenderInlineOptions {
-	component?: any
+  component?: any
 }
 
 // Plugin Options
 export interface ElementPluginOptions extends RenderElementOptions {}
 export interface MarkPluginOptions extends RenderInlineOptions {
-	hotkey?: string
+  hotkey?: string
 }
 export interface InlinePluginOptions extends RenderInlineOptions {
-	hotkey?: string
+  hotkey?: string
 }
