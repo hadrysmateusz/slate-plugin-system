c006efc7b71cc2c60cfffd3dd6c02f62fedc3ea0
Mateusz Hadryś <hadrys.mateusz86@gmail.com>
2020-03-28 13:33:58 +0100

    Remove node_modules
    Oops

--- packages/core/src/utilities/composeOverrides.ts
+++ packages/core/src/utilities/composeOverrides.ts
@@ -1,8 +1,12 @@
 import { Editor } from "slate"
-
 import { EditorOverrides } from "../types"
 
-export const composeOverrides = (overridesList: EditorOverrides[]): EditorOverrides => {
+/**
+ * Takes a list of EditorOverrides functions and returns a function 
+ * that calls them all from right to left
+ * @param overridesList List of EditorOverrides functions
+ */
+export const composeOverrides = (overridesList: EditorOverrides[]): EditorOverrides<Editor> => {
   return <T extends Editor>(editor: T) => {
     let e = editor
     overridesList.reverse().forEach((withOverrides) => {
